a2a272feb5e6df1cf5054fd2ca7d3c8476df63db
Jacob Juul <jacob.Juul@gmail.com>
2014-06-19 20:47:05 +0200

    more validations and functional tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,16 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	validates :title, uniqueness: true
+	validates :title, uniqueness: true 
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'image must be GIF, JPG or PNG'
 	}
+	validates_length_of :title, minimum: 3, too_short: 'Must be more than 3 characters'
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
+	
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -15,7 +15,7 @@ class ProductTest < ActiveSupport::TestCase
 
 	test "product price must be positive" do
 		product = Product.new(	title: 	products(:ruby).title,
-								description: "yyy"
+								description: "yyy",
 								image_url: "zzz.jpg")
 		product.price = -1
 		assert product.invalid?
@@ -24,17 +24,17 @@ class ProductTest < ActiveSupport::TestCase
 
 		product.price = 0
 		assert product.invalid?
-		assert equal ["must be greater than or equal to 0.01"],
+		assert_equal ["must be greater than or equal to 0.01"],
 		product.errors[:price]
 
 		product.price = 1
-		assert product.valid?
+		assert product.valid?, '#{product} should be valid'
 	end
 
 	def new_product(image_url)
 		Product.new(	title: "Title of the product",
-						description: "yyy"
-						price: 2,
+						description: "yyy",
+						price: 2.0,
 						image_url: image_url)
 	end
 
@@ -42,11 +42,12 @@ class ProductTest < ActiveSupport::TestCase
 		ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
              http://a.b.c/x/y/z/fred.gif }
     	bad = %w{ fred.doc fred.gif/more fred.gif.more }
-    ok.each do |name|
-    	assert new_product(name).valid?, "#{name} should be valid"
+	    ok.each do |name|
+	    	assert new_product(name).valid?, "#{name} should be valid"
+		end
+		bad.each do |name|
+			assert new_product(name).invalid?, "#{name} shouldn't be valid"
+		end		
 	end
-	bad.each do |name|
-		assert new_product(name).invalid?, "#{name} shouldn't be valid"
-	end		
 
 end

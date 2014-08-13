ec5cc24004ebce2f2df21f4f91d23553ee79a04f
Martin Bendsoe <martin.bendsoe@gmail.com>
2014-08-13 20:37:08 +0200

    changed the register page and added nice error message support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,21 @@ module ApplicationHelper
 		content_tag("div", attributes, &block)
 	end
 	
+  
+  def show_non_blank(value, before=" ", after=" ")
+    if value.blank?
+      " "
+    else
+      before + value + after if !value.blank?
+    end
+  end
+  
+  def get_errors( error_array, default )
+    if error_array.blank?
+      default
+    else
+      error_array.join(" ")
+    end
+  end
+  
 end

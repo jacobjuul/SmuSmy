31d27bebf89ecfdf07dc04cf8616f465fba3b383
Jacob Juul Christiansen <jacob.juul@gmail.com>
2014-08-14 14:10:28 +0200

    adds quantity to the products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730174540) do
+ActiveRecord::Schema.define(version: 20140814120845) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20140730174540) do
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity"
   end
 
   create_table "users", force: true do |t|

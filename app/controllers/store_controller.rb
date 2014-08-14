b70e60623f02b2bc4af91372f0305376748013e5
Jacob Juul Christiansen <jacob.juul@gmail.com>
2014-08-14 12:26:57 +0200

    better sql in store_controller

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,7 +5,8 @@ before_action :set_cart
 
   def index
 
-  	@frontpage = Category.find(9).products
+  	#Fetch all the products in the Front Page Category
+  	@frontpage = Product.joins(:categories).where(:categories =>{:name => 'Front Page'})
 
   	@products = Product.order(:title)
   	@user = User.name
